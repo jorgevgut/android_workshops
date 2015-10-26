71591ce1719da730d771decca2897b8ebadab01d
Jorge A Villalobos <jorge.villalobos.gutierrez@intel.com>
2015-10-26 14:09:56 -0600

    adding comments

--- app/src/main/java/me/jorge/practica1/MainActivity.java
+++ app/src/main/java/me/jorge/practica1/MainActivity.java
@@ -23,12 +23,32 @@ public class MainActivity extends AppCompatActivity  {
 
         Button button = (Button)findViewById(R.id.btn1);
 
+        //to manually add an event listener use
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //click implementation, remember that the "view" parameter is the source of the event
+//            }
+//        });
+
+
+        //Use this to delay starting the activity by 4 seconds
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                //use getApplicationContext to get Activity's instance, "this" does not work
+//                //since we are being encapsulated by the anonymous Runnable's instance
+//                Intent i = new Intent(getApplicationContext(),SettingsActivity.class);
+//                startActivity(i);
+//            }
+//        },4000); //4 second delay in ms
 
     }
 
+    //this method is being called since the onClick attribute is being set on activity_main.xml parameters
     public void evento(View v) {
 
-        switch (v.getId()) {
+        switch (v.getId()) { //check source by checking the id defined in the layout file
             case R.id.btn1:
                 Intent i = new Intent(this,SettingsActivity.class);
                 i.putExtra(Datos.KEY_NAME,"Jorge");
